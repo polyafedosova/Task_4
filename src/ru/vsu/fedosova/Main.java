b7ae668e3724fd5480ed39a0097a6eaced6bbc1e
Polina Fedosova <polya.fedosova74@gmail.com>
2020-10-27 12:55:07 +0300

    Correct the error

--- src/ru/vsu/fedosova/Main.java
+++ src/ru/vsu/fedosova/Main.java
@@ -3,46 +3,44 @@ package ru.vsu.fedosova;
 import java.util.Scanner;
 import java.util.Locale;
 
-public class Main {
+public class Main
+{
 
     public static void main(String[] args)
     {
         Locale.setDefault(Locale.ROOT);
 
-        int n = readQuantityAndDividend("n");
-        int divisor_1 = readQuantityAndDividend("divisor_1");
-        int divisor_2 = readQuantityAndDividend("divisor_2");
+        int n = readNumber("n");
+        int a = readNumber("a");
+        int b = readNumber("b");
 
-        int sum = sumNumbers(n, divisor_1, divisor_2);
+        int sum = sumNumbers(n, a, b);
 
         printSum(sum);
     }
 
-    static int readQuantityAndDividend(String number)
+    static int readNumber(String number)
     {
         Scanner scanner = new Scanner(System.in);
         System.out.printf("Input %s: ", number);
         return scanner.nextInt();
     }
 
-    public static int meetConditions(int divisor_1, int divisor_2, int number)
+    public static boolean meetConditions(int a, int b, int x)
     {
-        if ((number > 0) && (number % divisor_1 == 0) && (number % divisor_2 != 0))
-        {
-            return number;
-        }
-        return 0;
+        return (x > 0) && (x % a == 0) && (x % b != 0);
     }
 
-    public static int sumNumbers(int n, int divisor_1, int divisor_2)
+    public static int sumNumbers(int n, int a, int b)
     {
         int sum = 0;
         int count = 0;
         for(int i = 0; count < n; i++)
         {
-            int number = meetConditions(divisor_1, divisor_2, i);
-            sum = sum + number;
-            if(number != 0) count++;
+            if (meetConditions(a, b, i)) {
+                sum = sum + i;
+                count++;
+            }
         }
         return sum;
     }
